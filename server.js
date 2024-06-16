8c3d3239aa2bfe8c4f5d7ae5a4fc4a26977c9a99
Vitaliy Rubachuk <rubachuk288@gmail.com>
2024-06-16 13:50:26 +0300

    weg

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const crypto = require('crypto');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
