3fe9b59516d1413a56699c1d9fb65e4e3d7ca013
Vitaliy Rubachuk <rubachuk288@gmail.com>
2024-06-16 13:15:15 +0300

    35

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = process.env.PORT || 3000;
